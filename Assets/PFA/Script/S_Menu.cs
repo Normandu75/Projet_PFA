d3430b60bd0755a360ddc9b082f88ea0f3f11cc8
Normandu75 <dufavet.norman@outlook.fr>
2026-06-18 00:07:36 +0200

    Ajout du fbx du menu principal avec ses textures

--- Assets/PFA/Script/S_Menu.cs
+++ Assets/PFA/Script/S_Menu.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class S_Menu : MonoBehaviour
 {
+    public Animator anim;
+
     public void LaunchGame()
     {
+        StartCoroutine(WaitAnim());
+    }
+
+    IEnumerator WaitAnim()
+    {
+        anim.SetTrigger("Play");
+        
+        yield return new WaitForSeconds(4f);
+
         SceneManager.LoadSceneAsync(1);
     }
 }
