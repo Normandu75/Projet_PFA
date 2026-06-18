f876c30257cb3e1081cdeca12816cc64298e28e3
Normandu75 <dufavet.norman@outlook.fr>
2026-06-18 16:03:08 +0200

    Ajout des textures du cryopod et correction du script du menu
    désactivation et activation du gameobject de cryopod et anim cryo

--- Assets/PFA/Script/S_Menu.cs
+++ Assets/PFA/Script/S_Menu.cs
@@ -5,16 +5,28 @@ using UnityEngine.SceneManagement;
 public class S_Menu : MonoBehaviour
 {
     public Animator anim;
+    public GameObject cryoPod;
+    public GameObject cryoPodText;
 
     public void LaunchGame()
     {
         StartCoroutine(WaitAnim());
     }
 
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
     IEnumerator WaitAnim()
     {
-        anim.SetTrigger("Play");
         
+        cryoPod.gameObject.SetActive(true);
+
+        cryoPodText.gameObject.SetActive(false);
+
+        anim.SetTrigger("Play");
+
         yield return new WaitForSeconds(6f);
 
         SceneManager.LoadSceneAsync(1);
